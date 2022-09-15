006c7820a495e036a238c54bf65b36389615933e
tanachok12 <tanachok_d@elearning.ac.th>
2022-09-15 22:32:04 +0700

    3.4

--- src/main/java/se331/rest/entity/Event.java
+++ src/main/java/se331/rest/entity/Event.java
@@ -1,12 +1,22 @@
 package se331.rest.entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+    @Id
+            @GeneratedValue(strategy  = GenerationType.IDENTITY)
     Long id;
     String category;
     String title;
